c748ab50d743160a3f3f0f9f6aa4006d00fd324e
Phillipe Smith <phsmithcc@gmail.com>
2024-10-24 15:21:47 -0300

    feat: add eslint

---
+++ app/eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+];
